b090f413db969a44c324d6b7da4f03008a30fef6
Irving Salazar <ISCIrvingDev@gmail.com>
2025-06-02 14:35:01 -0600

    Layout: HeroBanner & SearchExercises Sections

---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ExerciseDetail = () => {
+  return (
+    <div>xxx</div>
+  )
+}
+
+export default ExerciseDetail
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { Box } from '@mui/material'
+
+// * Components
+import HeroBanner from '../components/HeroBanner'
+import SearchExercises from '../components/SearchExercises'
+import Exercise from '../components/Exercise'
+
+const Home = () => {
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercise />
+    </Box>
+  )
+}
+
+export default Home
