a8280a164caf942c0ef10fbbc9d47898baa734e0
Irving Salazar <ISCIrvingDev@gmail.com>
2025-06-05 20:59:36 -0600

    Layout:: Footer Section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,36 @@
 import React from "react";
+import { Box, Stack, Typography } from '@mui/material'
+
+import Logo from '../assets/images/Logo-1.png'
 
 const Footer = () => {
   return (
-    <div>xxx</div>
+    <Box
+      mt="80px"
+      bgcolor="#FFF3F4"
+    >
+      <Stack
+        gap="40px"
+        alignItems="center"
+        px="40px"
+        pt="24px"
+      >
+        <img
+          src={Logo}
+          alt="Logo"
+          width="200px"
+          height="40px"
+        />
+
+        <Typography
+          variant="h5"
+          pb="40px"
+          mt="20px"
+        >
+          Developed & Designed by <a href="/">Irving Salazar</a>
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
